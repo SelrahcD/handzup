d32cbdf4ed6c12751d293d0890dc0047a2336bf5
SelrahcD <c.desneuf@gmail.com>
2020-03-19 15:13:03 +0100

    Setup socket connection

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,8 +14,7 @@ import "phoenix_html"
 // Import local files
 //
 // Local files can be imported directly using relative paths, for example:
-// import socket from "./socket"
-
+import socket from "./socket"
 import VueRouter from "vue-router";
 import Vue from "vue";
 import Home from './components/Home.vue'
--- lib/handzup_web/channels/user_socket.ex
+++ lib/handzup_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule HandzupWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", HandzupWeb.RoomChannel
+   channel "room:*", HandzupWeb.RoomChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
